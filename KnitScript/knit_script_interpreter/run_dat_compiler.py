8cee6ab400a91903aa5d902656682f60b8703e57
mkhofmann <hofmann.megan@gmail.com>
2022-12-12 12:02:34 -0500

    Working on making this an installable package

--- KnitScript/knit_script_interpreter/run_dat_compiler.py
+++ KnitScript/knit_script_interpreter/run_dat_compiler.py
@@ -1,27 +1,28 @@
 """used to run js DAT compiler"""
 
 import os
+import subprocess
 from typing import Optional
 
 from Naked.toolshed.shell import execute_js
 
 
-def knitout_to_dat(knitout_file_name: str, dat_file_name: Optional[str] = None) -> bool:
+def knitout_to_dat(knitout_file_name: str, dat_file_name: Optional[str] = None, js_compiler_file:Optional[str] = None) -> bool:
     """
     Creates a dat file for the corresponding knitout
     Parameters
     ----------
     knitout_file_name: the filename of the knitout to compile
     dat_file_name: the dat filename to compile to. Defaults to the same as knitout
+    :param js_compiler_file: Specification for location of javascript code to convert knitout to dat
     """
-    cur_directory = os.path.dirname(__file__)
-    script_directory = f"{cur_directory}{os.path.sep}dat-compiler{os.path.sep}knitout-backend-swg-master"
-    script = f"{script_directory}{os.path.sep}knitout-to-dat.js"
-    script = f"{cur_directory}{os.path.sep}knitout-to-dat.js"
-    if dat_file_name is None:
-        knitout_name = knitout_file_name[:knitout_file_name.rindex(".")]
-        dat_file_name = f"{knitout_name}.dat"
+    directory = os.path.dirname(__file__)
+    # directory = 'C:\\Users\\Megan\\bin'
+    if js_compiler_file is None:
+        js_compiler_file = f"{directory}{os.path.sep}knitout-to-dat.js"
+
     arguments = f"{knitout_file_name} {dat_file_name}"
     print(f"################  Converting {knitout_file_name} to DAT FILE ########")
-    success = execute_js(script, arguments)
+    success = execute_js(js_compiler_file, arguments)
     return success
+
